26713a0c0a57960f83d7d9ae9e2c14103aa83211
adardev <adarlopz.dev@gmail.com>
2025-06-24 19:09:27 -0600

    otp de prueba con whatsapp API exitosa :)

--- src/pages/api/sendWhatsappTest.js
+++ src/pages/api/sendWhatsappTest.js
@@ -10,44 +10,26 @@ const client = new Twilio(accountSid, authToken);
 
 export async function POST({ request }) {
     const raw = await request.text();
-    let to, code;
+    console.log("CUERPO RAW:", raw);
 
+    let to, code;
     try {
         const data = JSON.parse(raw);
         to = data.to;
         code = data.code;
-
-        if (!to || !code) {
-            return new Response(
-                JSON.stringify({ error: "Faltan parámetros: 'to' o 'code'" }),
-                { status: 400, headers: { 'Content-Type': 'application/json' } }
-            );
-        }
-
     } catch (err) {
         return new Response(
-            JSON.stringify({ error: "Cuerpo JSON inválido" }),
+            JSON.stringify({ error: "JSON inválido" }),
             { status: 400, headers: { 'Content-Type': 'application/json' } }
         );
     }
-
     try {
         const message = await client.messages.create({
-            from: 'whatsapp:+15557991557', // Tu número habilitado por Meta
-            to: `whatsapp:${to}`,           // Número destino
-            template: {
-                name: 'verifications_2fa_template_hxac885d902c966d9cda3a577188a89fff', // Nombre de la plantilla en Meta
-                components: [
-                    {
-                        type: 'body',
-                        parameters: [
-                            { type: 'text', text: code } // Valor para {{1}}
-                        ]
-                    }
-                ]
-            }
+            from: 'whatsapp:+15557991557',
+            to: `whatsapp:${to}`,
+            contentSid: 'HXac885d902c966d9cda3a577188a89fff',
+            contentVariables: JSON.stringify({ "1": code })
         });
-
         return new Response(
             JSON.stringify({ sid: message.sid }),
             { status: 200, headers: { 'Content-Type': 'application/json' } }
