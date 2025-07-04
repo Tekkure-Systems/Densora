8b8cd750de5bf07251fb1ad89b4a30fefd16cdc6
adardev <adarlopz.dev@gmail.com>
2025-07-03 18:57:54 -0600

    Revert "pr"
    This reverts commit d09745b52fffe6ba6f1129b5736cadfd6b5dd2a5.

--- app/android/app/src/main/kotlin/com/tekkure/densora/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.tekkure.densora
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- web/src/services/otp.js
+++
@@ -1,3 +0,0 @@
-import { generateOtpCall } from "../firebaseFunctions/generateOtp.js";
-
-export const generateOtp = generateOtpCall;
