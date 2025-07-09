6b4457750764904522c4114936d95e41e1f05b5d
Emilio <madrigalemi31@gmail.com>
2025-07-08 19:17:28 -0600

    es mi cochinero espera, estoy reparando calendar que dejo de jalar

--- src/scripts/initCalendar.js
+++ src/scripts/initCalendar.js
@@ -3,19 +3,39 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 
-import { db } from '../bd/firebase.js'
-import { doc, collection, getDoc, getDocs } from 'firebase/firestore'
-
 const calendarEl = document.getElementById('calendar')
 
+if (calendarEl) {
+  const calendar = new Calendar(calendarEl, {
+    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+    initialView: 'dayGridMonth',
+    editable: false,
+    selectable: false,
+    droppable: false,
 
-    db.collection("pacientes").get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-          console.log(`${doc.id} => ${doc.data()}`);
-      });
-    });
-    
-    
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
 
-    
+    events: [
+      {
+        id: '1',
+        title: 'Reunión de equipo',
+        start: new Date().toISOString().split('T')[0],
+        color: '#1d4ed8',
+        textColor: '#fff'
+      }
+    ],
 
+    eventClick(info) {
+      info.jsEvent.preventDefault()
+      const { id, title, start } = info.event
+      const url = `/evento?id=${encodeURIComponent(id)}&title=${encodeURIComponent(title)}&start=${encodeURIComponent(start.toISOString())}`
+      window.location.href = url
+    }
+  })
+
+  calendar.render()
+}
