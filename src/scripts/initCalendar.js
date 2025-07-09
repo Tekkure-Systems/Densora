62bf6418598061bc1b2b22fbb3dc6fb62fe802d7
Emilio <madrigalemi31@gmail.com>
2025-07-09 11:36:32 -0600

    calendario esta mejor, falta diseno y firestore pero ya la volvi a reparar a su estado original

--- src/scripts/initCalendar.js
+++ src/scripts/initCalendar.js
@@ -1,14 +1,46 @@
-
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 
+
 document.addEventListener('DOMContentLoaded', () => {
+
+  let request="/eventos.json"
+
   const calendarEl = document.getElementById('calendar');
   if (calendarEl) {
     const calendar = new Calendar(calendarEl, {
       plugins: [dayGridPlugin, timeGridPlugin],
       initialView: 'dayGridMonth',
+
+      events:function(info,successCallback,failureCallback){
+        fetch(request)
+          .then(function(response){
+            return response.json()
+          })
+          .then(function(data){
+            let events =data.events.map(function(event){
+              return{
+                title: event.eventTitle,
+                start: new Date(event.eventStartDate),
+                end: new Date(event.eventEndDate),
+                url: event.eventUrl,
+                location: event.eventLocation,
+                timeStart: event.eventTimeStart,
+                timeEnd: event.eventTimeEnd
+              }
+            })
+
+            successCallback(events)
+
+          })
+          .catch(function(error){
+            failureCallback(error)
+          })
+              
+          
+      },
+
     });
     calendar.render();
   }
